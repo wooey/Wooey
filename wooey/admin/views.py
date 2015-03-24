d588bfee84ae7907b043b88ca36328e097f56e70
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2015-03-24 12:07:59 +0100

    Restrict admin to to is_admin users

--- wooey/admin/views.py
+++ wooey/admin/views.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import Blueprint, render_template
-from flask.ext.login import login_required
-from flask.ext import login
+from flask.ext.login import login_required, current_user
 
 from flask.ext.admin import Admin, BaseView, expose
 from flask.ext.admin.contrib.sqla import ModelView
@@ -10,17 +9,15 @@ from flask.ext.admin.contrib.sqla import ModelView
 class AdminView(BaseView):
 
     def is_accessible(self):
-        return login.current_user.is_authenticated()
+        return current_user.is_authenticated() and current_user.is_admin
 
-    @expose('/')
-    def index(self):
-        return self.render('index.html')
+class ModelView(ModelView, AdminView):
+    pass
 
 from ..extensions import flask_admin, db
-
 from ..user.models import User
-flask_admin.add_view(ModelView(User, db.session, endpoint='user-admin'))
-
 from ..public.models import Script, Job
+
+flask_admin.add_view(ModelView(User, db.session, endpoint='user-admin'))
 flask_admin.add_view(ModelView(Script, db.session, endpoint='script-admin'))
 flask_admin.add_view(ModelView(Job, db.session, endpoint='job-admin'))

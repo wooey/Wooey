e079daa7d081b680ccf566363b3788ee0e26b153
Chris Mitchell <chris.mit7@gmail.com>
2015-04-10 13:34:54 -0400

    fixed celery with program submission

--- djangui/conf/script_template/models/djangui_models.py
+++ djangui/conf/script_template/models/djangui_models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 from django.db import models
+from django.db.models.fields.files import FieldFile
 from ..tasks import submit_script
 
 class DjanguiModel(models.Model):
@@ -8,9 +9,23 @@ class DjanguiModel(models.Model):
 
     def save(self, *args, **kwargs):
         super(DjanguiModel, self).save(*args, **kwargs)
+        # In Celery, the filepaths are not always kept for some reason, so we process the fields here
         script_options = dict([(i.name, getattr(self, i.name)) for i in self._meta.fields])
-        script_options['djangui_model'] = self
-        submit_script(**script_options)
+        com = [script_options.pop('djangui_script_name')]
+        for i,v in script_options.iteritems():
+            param = self.get_option_param(i)
+            if param is None:
+                continue
+            if isinstance(v, FieldFile):
+                com += [param, v.path]
+            else:
+                if str(v) == 'True':
+                    com += [param]
+                elif str(v) == 'False':
+                    continue
+                else:
+                    com += [param, str(v)]
+        submit_script.delay(com)
 
     def get_option_param(self, param):
         # returns the param argparse accepts (ie --p vs -p)
--- djangui/conf/script_template/tasks.py
+++ djangui/conf/script_template/tasks.py
@@ -1,25 +1,25 @@
 from __future__ import absolute_import
-from celery import shared_task
-from django.db.models.fields.files import FieldFile
+from {{ project_name }}.celery import app
 
 import subprocess
 
-@shared_task
-def submit_script(**kwargs):
-    model = kwargs.pop('djangui_model')
-    com = [kwargs.pop('djangui_script_name')]
-    for i,v in kwargs.iteritems():
-        param = model.get_option_param(i)
-        if param is None:
-            continue
-        if isinstance(v, FieldFile):
-            com += [param, v.path]
-        else:
-            if str(v) == 'True':
-                com += [param]
-            elif str(v) == 'False':
-                continue
-            else:
-                com += [param, str(v)]
-    print com
-    subprocess.call(com)
+@app.task
+def submit_script(com, **kwargs):
+    # this doesn't work in here since we lose the filepaths
+    # model = kwargs.pop('djangui_model')
+    # com = [kwargs.pop('djangui_script_name')]
+    # for i,v in kwargs.iteritems():
+    #     param = model.get_option_param(i)
+    #     if param is None:
+    #         continue
+    #     if isinstance(v, FieldFile):
+    #         com += [param, v.path]
+    #     else:
+    #         if str(v) == 'True':
+    #             com += [param]
+    #         elif str(v) == 'False':
+    #             continue
+    #         else:
+    #             com += [param, str(v)]
+    # print com
+    proc = subprocess.call(com)

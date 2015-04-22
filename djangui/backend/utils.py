1ec51c78e995c3dcebd4dd760ba4a1384806c1c2
Chris Mitchell <chris.mit7@gmail.com>
2015-04-22 11:40:03 -0400

    cosmetics

--- djangui/backend/utils.py
+++ djangui/backend/utils.py
@@ -2,13 +2,12 @@ __author__ = 'chris'
 from django.core.urlresolvers import reverse_lazy, reverse
 
 def sanitize_name(name):
-    return name.replace(' ', '_').replace('-','_')
+    return name.replace(' ', '_').replace('-', '_')
 
 def sanitize_string(value):
     return value.replace('"', '\\"')
 
 def get_djangui_model_json_url(app, model_name):
-    print app, model_name
     return reverse_lazy('{0}_script_json'.format(app), kwargs={'script_name': model_name})
 
 def is_djangui_model(model):

2dfcb9586f331eb0d14372540ca01d706fb22497
Chris Mitchell <chris.mit7@gmail.com>
2015-04-22 00:57:09 -0400

    removed old urls

--- djangui/conf/project_template/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-
-urlpatterns = [
-    #url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('djguicore.urls')),
-    url(r'^', include('{{ app_name }}.urls')),
-]

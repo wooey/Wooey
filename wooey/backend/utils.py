261ec6922136c022718b8a890901e63c3b91e0a2
Chris Mitchell <chris.mit7@gmail.com>
2015-08-12 10:15:19 -0400

    Bugfix: invalid method of stderr

--- wooey/backend/utils.py
+++ wooey/backend/utils.py
@@ -351,7 +351,7 @@ def create_job_fileinfo(job):
             else:
                 files.append(d)
         except IOError:
-            sys.stderr.format('{}'.format(traceback.format_exc()))
+            sys.stderr.write('{}'.format(traceback.format_exc()))
             continue
 
     # establish grouping by inferring common things

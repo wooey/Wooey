99c77c855782434bcb4f97b589887aa1b0ee121d
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2015-08-13 15:49:02 +0200

    Add DJANGO_SETTINGS_MODULE to doc conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -51,7 +51,9 @@ MOCK_MODULES = [
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
-    
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'wooey.settings'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.

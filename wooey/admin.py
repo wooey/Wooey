794bf766c195c3a403d56410d1cd83e05f091592
Chris Mitchell <chris.mit7@gmail.com>
2015-09-07 10:30:08 -0400

    Enforce a singular default_version for ScriptVersion and removed extra fields that were confusing from ScriptVersion admin

--- wooey/admin.py
+++ wooey/admin.py
@@ -1,13 +1,28 @@
 from __future__ import absolute_import
 from django.contrib.admin import ModelAdmin, site, TabularInline
+from django.forms import ModelForm, ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from .models import Script, ScriptVersion, ScriptGroup, ScriptParameter, WooeyJob, ScriptParameterGroup, WooeyFile
+
+class ScriptVersionForm(ModelForm):
+    class Meta:
+        model = ScriptVersion
+        fields = '__all__'
+
+    def clean(self):
+        cleaned = self.cleaned_data
+        # make sure we only have 1 default
+        current_defaults = ScriptVersion.objects.filter(script=cleaned['script'], default_version=True)
+        current_defaults.update(default_version=False)
 
 class JobAdmin(ModelAdmin):
     list_display = ('user', 'job_name', 'script_version', 'status', 'created_date')
 
 class ScriptVersionInline(TabularInline):
     model = ScriptVersion
+    extra = 0
+    form = ScriptVersionForm
 
 class ScriptAdmin(ModelAdmin):
     list_display = ('script_name', 'script_group', 'is_active')

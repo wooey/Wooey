2409dfabbd53aaa9e21d8b93485cff0dd2db308c
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2015-09-08 12:10:09 +0200

    Remove migrations.RunPython.noop from reverse for Django<1.8

--- wooey/migrations/0012_wooeyjob_uuid.py
+++ wooey/migrations/0012_wooeyjob_uuid.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
             field=models.CharField(default=uuid.uuid4, unique=False, max_length=255),
         ),
         # Set the uuids for existing records
-        migrations.RunPython(gen_uuid, reverse_code=migrations.RunPython.noop),
+        migrations.RunPython(gen_uuid),
         # Set to unique=True 
         migrations.AlterField(
             model_name='wooeyjob',

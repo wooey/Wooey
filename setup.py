aaa11770b11581fa454cb8cbda556a982ba35ec9
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2015-09-08 20:31:28 +0200

    Version 0.9.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='wooey',
-    version='0.2.6',
+    version='0.9.0',
     packages=find_packages(),
     scripts=['scripts/wooify'],
     entry_points={'console_scripts': ['wooify = wooey.backend.command_line:bootstrap', ]},

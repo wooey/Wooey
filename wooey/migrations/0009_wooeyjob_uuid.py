9928136681629b6d3e49bdd15839d1caad3feaf2
Martin Fitzpatrick <martin.fitzpatrick@gmail.com>
2015-09-08 11:04:56 +0200

    Add migration for UUID (pre-fill)

---
+++ wooey/migrations/0009_wooeyjob_uuid.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import uuid, os
+
+def gen_uuid(apps, schema_editor):
+    WooeyJob = apps.get_model('wooey', 'WooeyJob')
+    for obj in WooeyJob.objects.all():
+        obj.uuid = uuid.uuid4()
+        obj.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wooey', '0008_short_param_admin'),
+    ]
+
+    operations = [
+        # Add the uuid field with unique=False for existing entries
+        # due to a bug in migrations this will set all to the same uuid
+        migrations.AddField(
+            model_name='wooeyjob',
+            name='uuid',
+            field=models.CharField(default=uuid.uuid4, unique=False, max_length=255),
+        ),
+        # Set the uuids for existing records
+        migrations.RunPython(gen_uuid, reverse_code=migrations.RunPython.noop),
+        # Set to unique=True 
+        migrations.AlterField(
+            model_name='wooeyjob',
+            name='uuid',
+            field=models.CharField(default=uuid.uuid4, unique=True, max_length=255),
+        ),
+    ]
+

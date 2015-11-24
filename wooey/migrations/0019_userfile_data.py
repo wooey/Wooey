08b8a9ebe420ea795026eedb9cdbfb1c7ed3ec46
Chris Mitchell <chris.mit7@gmail.com>
2015-11-24 17:58:34 -0500

    migration fix

--- wooey/migrations/0019_userfile_data.py
+++ wooey/migrations/0019_userfile_data.py
@@ -20,11 +20,19 @@ def gen_userfiles(apps, schema_editor):
     ctype = ContentType.objects.get(model='wooeyfile')
     new_ctype = ContentType.objects.get(model='userfile')
     import os
+    checksums = {}
+    to_delete = []
     for obj in WooeyFile.objects.all():
+        checksum = obj.checksum
+        first_file = checksums.get(checksum, obj)
         user_file = UserFile(filename=os.path.split(obj.filepath.name)[1], job=obj.job,
-                             parameter=obj.parameter, system_file=obj)
+                             parameter=obj.parameter, system_file=first_file)
         user_file.save()
-        Favorite.objects.filter(content_type=ctype, object_id=obj.id).update(content_object=obj, content_type=new_ctype)
+        Favorite.objects.filter(content_type=ctype, object_id=obj.id).update(content_object=first_file, content_type=new_ctype)
+        if first_file != obj:
+            to_delete.append(obj.pk)
+    # remove redundant wooeyfiles
+    WooeyFile.objects.filter(pk__in=to_delete).delete()
 
 
 class Migration(migrations.Migration):

ee94bb4f17f53e57545d18b5a20ce3a4b7bec142
Chris Mitchell <chris.mit7@gmail.com>
2016-03-09 09:10:01 -0500

    setings reorganized

--- wooey/settings.py
+++ wooey/settings.py
@@ -7,18 +7,25 @@ from django.utils.translation import ugettext_lazy as _
 def get(key, default):
     return getattr(settings, key, default)
 
-WOOEY_FILE_DIR = get('WOOEY_FILE_DIR', 'wooey_files')
-WOOEY_SCRIPT_DIR = get('WOOEY_SCRIPT_DIR', 'wooey_scripts')
-WOOEY_CELERY = get('WOOEY_CELERY', True)
-WOOEY_CELERY_TASKS = get('WOOEY_CELERY_TASKS', 'wooey.tasks')
+# AUTH based settings
 WOOEY_ALLOW_ANONYMOUS = get('WOOEY_ALLOW_ANONYMOUS', True)
 WOOEY_AUTH = get('WOOEY_AUTH', True)
 WOOEY_LOGIN_URL = get('WOOEY_LOGIN_URL', settings.LOGIN_URL)
 WOOEY_REGISTER_URL = get('WOOEY_REGISTER_URL', '/accounts/register/')
-WOOEY_SHOW_LOCKED_SCRIPTS = get('WOOEY_SHOW_LOCKED_SCRIPTS', True)
-WOOEY_EPHEMERAL_FILES = get('WOOEY_EPHEMERAL_FILES', False)
+
+# Celery and job queue settings
+WOOEY_CELERY = get('WOOEY_CELERY', True)
+WOOEY_CELERY_TASKS = get('WOOEY_CELERY_TASKS', 'wooey.tasks')
+
+# Site setup settings
 WOOEY_DEFAULT_SCRIPT_GROUP = get('WOOEY_DEFAULT_SCRIPT_GROUP', _('Scripts'))
+WOOEY_EPHEMERAL_FILES = get('WOOEY_EPHEMERAL_FILES', False)
+WOOEY_FILE_DIR = get('WOOEY_FILE_DIR', 'wooey_files')
+WOOEY_REALTIME_CACHE = get('WOOEY_REALTIME_CACHE', None)
+WOOEY_SCRIPT_DIR = get('WOOEY_SCRIPT_DIR', 'wooey_scripts')
+
+# User interface settings
+WOOEY_SHOW_LOCKED_SCRIPTS = get('WOOEY_SHOW_LOCKED_SCRIPTS', True)
 WOOEY_SITE_NAME = get('WOOEY_SITE_NAME', _('Wooey!'))
 WOOEY_SITE_TAG = get('WOOEY_SITE_TAG', _('A web UI for Python scripts'))
 
-WOOEY_REALTIME_CACHE = get('WOOEY_REALTIME_CACHE', None)

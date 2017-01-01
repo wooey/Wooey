82f28247f4fcf29af5e5a8037fe35a56431227f9
Chris Mitchell <chris.mit7@gmail.com>
2017-01-01 13:44:17 -0500

    Compatility with django1.6-1.9 on bootstrap

--- wooey/__init__.py
+++ wooey/__init__.py
@@ -3,7 +3,7 @@ import os
 if version.DJANGO_VERSION >= version.DJ17:
     default_app_config = 'wooey.apps.WooeyConfig'
 else:
-    if os.environ.get('TESTING') != 'True':
+    if os.environ.get('TESTING') != 'True' and os.environ.get('WOOEY_BOOTSTRAP') != 'True':
         from . import settings as wooey_settings
         # we need to call from within wooey_settings so the celery/etc vars are setup
         if not wooey_settings.settings.configured:

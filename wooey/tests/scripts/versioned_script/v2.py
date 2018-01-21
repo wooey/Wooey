dec9f58d56f25a43ff4d342540508dde418b2e6a
Chris Mitchell <chris@ginkgobioworks.com>
2018-01-21 14:28:32 -0500

    Add some tests for script versions

---
+++ wooey/tests/scripts/versioned_script/v2.py
@@ -0,0 +1,10 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="This is version 2")
+parser.add_argument('--one')
+parser.add_argument('--two')
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    print(args)

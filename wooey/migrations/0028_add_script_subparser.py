387be4187ed8c934a4d8f577d4bc48c80c6271f1
Chris Mitchell <chris.mit7@gmail.com>
2018-03-29 08:34:04 -0700

    Update migrations (#227)
    * Update migrations
    
    * Fix english

--- wooey/migrations/0028_add_script_subparser.py
+++ wooey/migrations/0028_add_script_subparser.py
@@ -29,7 +29,7 @@ class Migration(migrations.Migration):
             name='ScriptParser',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(blank=True, max_length=255, null=True)),
+                ('name', models.CharField(blank=True, max_length=255, default='')),
                 ('script_version', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='wooey.ScriptVersion')),
             ],
             bases=(wooey.models.mixins.WooeyPy2Mixin, models.Model),

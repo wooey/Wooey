9e706341fafc8a931a662ea497df39fff6f9408b
Chris Mitchell <chris@ginkgobioworks.com>
2018-04-15 14:49:54 -0400

    Add in shim nullable FK for initial scriptparameter update

--- wooey/migrations/0028_add_script_subparser.py
+++ wooey/migrations/0028_add_script_subparser.py
@@ -26,7 +26,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='scriptparameter',
             name='parser',
-            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='wooey.ScriptParser'),
+            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.CASCADE, to='wooey.ScriptParser'),
             preserve_default=False,
         ),
     ]

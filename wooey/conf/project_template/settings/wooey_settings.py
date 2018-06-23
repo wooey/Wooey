a8a17496a4baa52c4f82b9ad23ede03ead934f61
partrita <partrita@gmail.com>
2018-06-23 15:01:14 +0900

    fix locale and edit wooey_settings.py

--- wooey/conf/project_template/settings/wooey_settings.py
+++ wooey/conf/project_template/settings/wooey_settings.py
@@ -9,21 +9,24 @@ INSTALLED_APPS += (
 
 if DJANGO_VERSION < DJ110:
     MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
-    MIDDLEWARE_CLASSES.append('{{ project_name }}.middleware.ProcessExceptionMiddleware')
+    MIDDLEWARE_CLASSES.append(
+        '{{ project_name }}.middleware.ProcessExceptionMiddleware')
     MIDDLEWARE_OBJ = MIDDLEWARE_CLASSES
 else:
     # Using Django 1.10 +
     MIDDLEWARE = list(MIDDLEWARE)
-    MIDDLEWARE.append('{{ project_name }}.middleware.ProcessExceptionMiddleware')
+    MIDDLEWARE.append(
+        '{{ project_name }}.middleware.ProcessExceptionMiddleware')
     MIDDLEWARE_OBJ = MIDDLEWARE
 
 LANGUAGES = [
-  ('de', _('German')),
-  ('en', _('English')),
-  ('fr', _('French')),
-  ('ja', _('Japanese')),
-  ('nl', _('Dutch')),
-  ('zh-hans', _('Simplified Chinese')),
+    ('de', _('German')),
+    ('en', _('English')),
+    ('fr', _('French')),
+    ('ja', _('Japanese')),
+    ('nl', _('Dutch')),
+    ('zh-hans', _('Simplified Chinese')),
+    ('ko', _('Korean')),
 ]
 
 NEW_MIDDLEWARE = []
@@ -32,7 +35,8 @@ for i in MIDDLEWARE_OBJ:
     if i == 'django.contrib.sessions.middleware.SessionMiddleware':
         NEW_MIDDLEWARE.append('django.middleware.locale.LocaleMiddleware')
 
-NEW_MIDDLEWARE.append('{{ project_name }}.middleware.ProcessExceptionMiddleware')
+NEW_MIDDLEWARE.append(
+    '{{ project_name }}.middleware.ProcessExceptionMiddleware')
 if DJANGO_VERSION < DJ110:
     MIDDLEWARE_CLASSES = NEW_MIDDLEWARE
 else:

f06285387b7f78bf2c5d84faff4f986bc0979f86
Chris Mitchell <chris.mit7@gmail.com>
2019-03-24 20:44:31 -0400

    Update admin url

--- wooey/conf/project_template/urls/wooey_urls.py
+++ wooey/conf/project_template/urls/wooey_urls.py
@@ -2,7 +2,7 @@ from .django_urls import *
 from django.conf.urls import include, url
 
 urlpatterns += [
-    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', admin.site.urls),
     url(r'^', include('wooey.urls')),
     url(r'^', include('django.contrib.auth.urls')),
 ]

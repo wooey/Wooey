5477feb9de77d9b8b1328b6724c935b5eba1e221
Chris Mitchell <chris.mit7@gmail.com>
2019-11-29 21:43:36 -0500

    Upgrade to use boto3

--- wooey/wooeystorage.py
+++ wooey/wooeystorage.py
@@ -2,15 +2,14 @@ from __future__ import absolute_import
 
 import os
 
-from boto.utils import parse_ts
 from django.core.files.storage import get_storage_class, FileSystemStorage
-from storages.backends.s3boto import S3BotoStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 from . import settings as wooey_settings
 
 # From https://github.com/jezdez/django_compressor/issues/100
 
-class CachedS3BotoStorage(S3BotoStorage):
+class CachedS3Boto3Storage(S3Boto3Storage):
     def __init__(self, *args, **kwargs):
         if os.environ.get('TESTING', False):
             from .tests import config
@@ -23,14 +22,6 @@ class CachedS3BotoStorage(S3BotoStorage):
         if name.endswith('.gz'):
             return original_file
         return original_file
-
-    def modified_time(self, name):
-        name = self._normalize_name(self._clean_name(name))
-        entry = self.entries.get(name)
-        if entry is None:
-            entry = self.bucket.get_key(self._encode_name(name))
-        # Parse the last_modified string to a local datetime object.
-        return parse_ts(entry.last_modified)
 
     def path(self, name):
         return self.local_storage.path(name)

d714c94ddde9f70e876d5784c9dde53b0ceef31d
Chris Mitchell <chris.mit7@gmail.com>
2019-11-29 21:58:53 -0500

    fix super class

--- wooey/wooeystorage.py
+++ wooey/wooeystorage.py
@@ -12,11 +12,11 @@ class CachedS3Boto3Storage(S3Boto3Storage):
         if os.environ.get('TESTING', False):
             from .tests import config
             kwargs['location'] = config.WOOEY_TEST_REMOTE_STORAGE_DIR
-        super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
+        super(CachedS3Boto3Storage, self).__init__(*args, **kwargs)
         self.local_storage = get_storage_class('django.core.files.storage.FileSystemStorage')()
 
     def _open(self, name, mode='rb'):
-        original_file = super(CachedS3BotoStorage, self)._open(name, mode=mode)
+        original_file = super(CachedS3Boto3Storage, self)._open(name, mode=mode)
         if name.endswith('.gz'):
             return original_file
         return original_file
@@ -27,7 +27,7 @@ class CachedS3Boto3Storage(S3Boto3Storage):
     def delete(self, name):
         # we have to remove the name from the _entries cache or else deleted files will persist in our cache
         # and give false information
-        super(CachedS3BotoStorage, self).delete(name)
+        super(CachedS3Boto3Storage, self).delete(name)
         name = self._normalize_name(self._clean_name(name))
         encoded_name = self._encode_name(name)
         self._entries.pop(encoded_name, None)

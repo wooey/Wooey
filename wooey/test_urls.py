30aac0c2c1174de4cbe7579d7fabddb667b22cc2
Chris Mitchell <chris.mit7@gmail.com>
2020-04-22 15:50:08 -0400

    Namespace

--- wooey/test_urls.py
+++ wooey/test_urls.py
@@ -1,13 +1,15 @@
 from . import test_settings
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include, path, url
 from django.conf.urls.static import static
+from django.contrib import admin
 
 # the DEBUG setting in test_settings is not respected
 settings.DEBUG = True
 
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     url(r'^', include('wooey.urls')),
     url(r'^', include('django.contrib.auth.urls')),
 ]

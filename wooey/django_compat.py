a55579d0bba69e686e4d8935a2592510b1e6c86f
Chris Mitchell <chris.mit7@gmail.com>
2021-08-07 13:39:13 -0400

    Remove more unused

--- wooey/django_compat.py
+++ wooey/django_compat.py
@@ -6,10 +6,5 @@ try:
 except RuntimeError:
     pass
 
-if DJANGO_VERSION < DJ19:
-    from django.template.base import TagHelperNode, parse_bits
-else:
-    from django.template.library import TagHelperNode, parse_bits
-
 from django.template import Engine
 get_template_from_string = Engine.get_default().from_string

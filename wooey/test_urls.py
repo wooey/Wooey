207b83be44f77552733ca2544d8410153bb9597f
Chris Mitchell <chris.mit7@gmail.com>
2021-08-07 12:47:57 -0400

    Fix path import

--- wooey/test_urls.py
+++ wooey/test_urls.py
@@ -1,6 +1,6 @@
 from . import test_settings
 from django.conf import settings
-from django.conf.urls import include, path, url
+from django.urls import include, path, url
 from django.conf.urls.static import static
 from django.contrib import admin
 

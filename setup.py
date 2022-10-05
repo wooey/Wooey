398931c72e4fe303a52e2ab233c53df024af5392
Chris Mitchell <chris@ginkgobioworks.com>
2022-10-05 09:36:58 -0400

    Bump importlib

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         'django-celery-results',
         'django-storages',
         'eventlet>=0.22.1 ;platform_system=="Windows"',
+        'importlib-metadata<5.0',
         'jsonfield<3',
         'pypiwin32 ;(platform_system=="Windows" and python_version>"3.4")',
         'six',

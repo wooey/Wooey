caee19cdfb1ffa2aebca778b112f4d5930c3f3ad
Chris Mitchell <chris.mit7@gmail.com>
2023-04-30 22:52:44 -0400

    Exclude wooey app dir from docs and remove django use in docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,16 +14,12 @@
 
 import sys
 import os
-import django
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'wooey.test_settings'
-django.setup()
 
 # -- General configuration ------------------------------------------------
 

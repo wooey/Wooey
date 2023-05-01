b572e58d1d9e19498b2f73fcf0f96e569d476398
Chris Mitchell <chris.mit7@gmail.com>
2023-04-30 22:25:23 -0400

    Remove unneeded wooey app from doc build

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,7 @@ release = '3.0.0'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'wooey/*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.

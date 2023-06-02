4ebec57d791f1d9b44f8c2431ab847330818ce79
Chris Mitchell <chris.mit7@gmail.com>
2023-06-02 09:47:00 -0400

    Remove python_version from setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=[
         "celery>=4.0,<5",
         "clinto>=0.3.0",
-        'Django>=3,<4 ;python_version>="3.7"',
+        "Django>=3,<5",
         "django-autoslug",
         "django-storages",
         'eventlet>=0.22.1 ;platform_system=="Windows"',
